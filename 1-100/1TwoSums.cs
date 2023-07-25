90d70d67717570290b58384aababb18c52d99c72
Tomas Venere <veneret@gmail.com>
2023-07-24 23:57:32 -0400

    Adding 1.3

--- 1-100/1TwoSums.cs
+++ 1-100/1TwoSums.cs
@@ -2,6 +2,12 @@
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //You can return the answer in any order.
 
+/*Example
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
 using System;
 using System.Collections.Generic;
 
