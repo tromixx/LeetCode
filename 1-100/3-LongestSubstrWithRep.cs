1becdb47486c98ddb0c698946fad32cd8ad0e266
Tommy Venere <veneret@gmail.com>
2023-07-28 00:30:07 -0400

    3 adding 4

--- 1-100/3-LongestSubstrWithRep.cs
+++ 1-100/3-LongestSubstrWithRep.cs
@@ -3,6 +3,9 @@
 Given a string s, find the length of the longest substring without repeating characters.
 
 Example:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 */
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
