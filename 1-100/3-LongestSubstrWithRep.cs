eccb8decf069a7134036c85c490973f2911d592f
Tommy Venere <veneret@gmail.com>
2023-07-29 16:41:29 -0400

    4 adding 3

--- 1-100/3-LongestSubstrWithRep.cs
+++ 1-100/3-LongestSubstrWithRep.cs
@@ -1,3 +1,4 @@
+//O(n)
 //Longest Substring Without Repeating Characters
 /*
 Given a string s, find the length of the longest substring without repeating characters.
--- 1-100/4-MedianOfTwoSortedArrays.cs
+++ 1-100/4-MedianOfTwoSortedArrays.cs
@@ -1,4 +1,21 @@
+//Time complexity: O(log(n+m))O(log(n+m))O(log(n+m))
+//Space complexity: O(1)O(1)O(1)
 /*
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
 */
+
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
+    {
+        List<int> nums = nums1.ToList();
+        nums.AddRange(nums2.ToList());
+        nums.Sort();
+        if(nums.Count % 2 == 1){
+            return nums[nums.Count/2];
+        }
+       
+        return (nums[nums.Count/2] + nums[nums.Count/2 - 1])/2.0;
+    }
+}
+
