c70b0e94e24e9c2818445789d7c5db483a1cbd6d
Tommy Venere <veneret@gmail.com>
2023-07-31 23:31:14 -0400

    6 adding 1

---
+++ 1-100/6-ZigzagConversion.cs
@@ -0,0 +1,12 @@
+/*
+The string [input]"PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: [output]"PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+*/
