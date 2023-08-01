6c014ce3540070fe59e1bd378377a93012283aae
Tommy Venere <veneret@gmail.com>
2023-08-01 00:18:15 -0400

    7 adding 2

--- 1-100/7-ReverseInteger.cs
+++ 1-100/7-ReverseInteger.cs
@@ -1,1 +1,32 @@
 //cositas
+public class Solution {
+    public int Reverse(int x) 
+    {
+        string forward = ToString(x);
+        string backwards = "";
+
+        if(x>0)
+        {
+            for(i = forward.Length - 1, i >= 0 ; i--)
+            {
+                backwards =+ forward[i];
+            }
+        }
+        else
+        {
+            x = x + x + x;
+            forward = ToString(x);
+
+            for(i = forward.Length - 1, i >= 0 ; i--)
+            {
+                backwards =+ forward[i];
+            }
+
+        }
+
+        int backwardsInt = Int32.Parse(backwards);
+
+        return backwardsInt;
+
+    }
+}
