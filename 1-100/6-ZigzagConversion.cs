7bc733d6ffc41a6da888b5252665f3daadaea8e0
Tommy Venere <veneret@gmail.com>
2023-07-31 23:44:11 -0400

    6 adding 2

--- 1-100/6-ZigzagConversion.cs
+++ 1-100/6-ZigzagConversion.cs
@@ -10,3 +10,30 @@ Write the code that will take a string and make this conversion given a number o
 
 string convert(string s, int numRows);
 */
+
+//Time complexity: O(n)
+//Space complexity: O(n)
+
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (numRows == 1)
+            return s;
+
+        var result = new StringBuilder();
+        var perIter = numRows*2 - 2;
+        for (int row = 0; row < numRows; row++){
+            int i = 0;
+            while ((i+row) < s.Length){
+                result.Append(s[i+row]);
+                if (row != 0  && row != numRows-1){
+                    if((i + perIter - row) < s.Length){
+                        result.Append(s[i + perIter - row]);
+                    }
+                }
+
+                i += perIter;
+            }
+        }
+        return result.ToString();
+    }
+}
