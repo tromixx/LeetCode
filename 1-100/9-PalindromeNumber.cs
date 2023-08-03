af3b9d50df91db930db34709ad3f78d57bea72dd
Tommy Venere <veneret@gmail.com>
2023-08-02 23:53:56 -0400

    9 adding 0

---
+++ 1-100/9-PalindromeNumber.cs
@@ -0,0 +1,8 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        //create p that is reverse of x
+        //compare if p = x, if it is true, otherwise false
+    }
+}
