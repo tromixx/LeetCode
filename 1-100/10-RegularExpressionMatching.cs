0dfce4c5910108fc3f9d2409d5d1fc9848a19f4a
Tommy Venere <veneret@gmail.com>
2023-08-04 01:20:21 -0400

    10 commit 2

--- 1-100/10-RegularExpressionMatching.cs
+++ 1-100/10-RegularExpressionMatching.cs
@@ -5,3 +5,49 @@ Given an input string s and a pattern p, implement regular expression matching w
 '*' Matches zero or more of the preceding element.
 The matching should cover the entire input string (not partial).
 */
+
+public class Solution {
+    public bool IsMatch(string s, string p) {
+        int pi = 0;
+        return IsMatchHelper(s, p, s.Length-1, p.Length-1);
+    }
+
+    private bool IsMatchHelper(string s, string p, int si, int pi){
+        if(si < 0){
+            while(pi >= 0 && p[pi] == '*'){
+                pi -= 2;
+            }
+
+            return pi < 0;
+        }
+
+        if(pi < 0){
+            return false;
+        }
+        
+        if(s[si] == p[pi] || p[pi] == '.'){
+            return IsMatchHelper(s, p, si-1, pi-1);
+        }
+
+        if(p[pi] != '*'){
+            return false;
+        }
+
+        while(pi >= 0 && p[pi] == '*'){
+            pi--;
+        }
+
+        if(pi < 0)
+            return false;
+
+        while(si >= 0 && (s[si] == p[pi] || p[pi] == '.')){
+            if(IsMatchHelper(s, p, si, pi-1)){
+                return true;
+            }
+
+            si--;
+        }
+        
+        return IsMatchHelper(s, p, si, pi-1);
+    }
+}
