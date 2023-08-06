92a599c4f380aa51f56528784a5aae38fe299192
Tommy Venere <veneret@gmail.com>
2023-08-06 13:18:38 -0400

    12 commit 2

--- 1-100/12-cositas.cs
+++ 1-100/12-cositas.cs
@@ -8,6 +8,83 @@ public class Solution {
         C             100
         D             500
         M             1000
+
+        3999 max
         */
+        var numStr = ToString(num);
+        var leng = numStr.Length() ;
+        var finalString = "";
+
+        while(leng != -1)
+        {
+            //for 1000 - 3000
+            if(leng == 3)
+            {
+                if(numStr[leng] == 1)
+                {
+                    finalString =+ 'M';
+                }
+
+                else if(numStr[leng] == 2)
+                {
+                    finalString =+ 'M' + 'M';
+                }
+
+                else
+                {
+                    finalString =+ 'M' + 'M' + 'M';
+                }
+            }
+
+            //for 1-9
+            if(leng == 0)
+            {
+                if(numStr[leng] == 1)
+                {
+                    finalString =+ 'I';
+                }
+
+                else if(numStr[leng] == 2)
+                {
+                    finalString =+ 'I' + 'I';
+                }
+
+                else if(numStr[leng] == 3)
+                {
+                    finalString =+ 'I' + 'I' + 'I';
+                }
+
+                else if(numStr[leng] == 4)
+                {
+                    finalString =+ 'I' + 'I' + 'I' + 'I';
+                }
+
+                else if(numStr[leng] == 5)
+                {
+                    finalString =+ 'V';
+                }
+
+                else if(numStr[leng] == 6)
+                {
+                    finalString =+ 'V' + 'I';
+                }
+
+                else if(numStr[leng] == 7)
+                {
+                    finalString =+ 'V' + 'I' + 'I';
+                }
+
+                else if(numStr[leng] == 8)
+                {
+                    finalString =+ 'V' + 'I' + 'I' + 'I';
+                }
+
+                else if(numStr[leng] == 9)
+                {
+                    finalString =+ 'V' + 'I' + 'I' + 'I' + 'I';
+                }
+            }
+            leng--;
+        }
     }
 }
