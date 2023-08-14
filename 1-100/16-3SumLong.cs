7150fd93818ed8c321a7f353abd2744314591009
Tommy Venere <veneret@gmail.com>
2023-08-13 23:32:09 -0400

    17 adding something 1

--- 1-100/16-3SumLong.cs
+++ 1-100/16-3SumLong.cs
@@ -12,3 +12,32 @@ Input: nums = [-1,2,1,-4], target = 1
 Output: 2
 Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
+
+public class Solution {
+    public int ThreeSumClosest(int[] nums, int target) {
+       int n=nums.Length;
+       int minAns=int.MaxValue;
+       int result=0;
+       Array.Sort(nums);
+       for(int i=0;i<n-2;i++)
+       {
+           int left=i+1;
+           int right=n-1;
+           while(left<right)
+           {
+               int values=nums[i]+nums[left]+nums[right];
+               int diffOfValAndTarget=Math.Abs(values-target);
+               if(minAns>diffOfValAndTarget)
+                  {
+                      result=values;
+                      minAns=diffOfValAndTarget;
+                  }
+              if(values>target)
+               right--;
+               else
+               left++;
+            }
+       }
+       return result;
+    }
+}
