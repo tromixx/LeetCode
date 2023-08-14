53c9aa0aac3cd15e394b4d63627d558046821e04
Tommy Venere <veneret@gmail.com>
2023-08-13 23:44:56 -0400

    17 adding something 2

--- 1-100/17-cositas.cs
+++ 1-100/17-cositas.cs
@@ -4,3 +4,20 @@ Return the answer in any order.
 
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 */
+
+public class Solution {        
+        public IList<string> LetterCombinations(string digits)
+        {
+            IList<string> result = new List<string>();
+            if(string.IsNullOrEmpty(digits))
+                return result;
+
+            private void Backtrack(IList<string> result, string current, string digits, int inderx){
+                if (index == digits.Length){
+                    result.Add(current);
+                    return;
+                }
+            }
+        }
+    }
+}
