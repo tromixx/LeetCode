5a6644effab9eef855df13a88843c6bf4c2966d4
Tommy Venere <veneret@gmail.com>
2023-08-13 23:53:45 -0400

    17 adding something 4

--- 1-100/17-cositas.cs
+++ 1-100/17-cositas.cs
@@ -5,6 +5,24 @@ Return the answer in any order.
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 */
 
+/*The problem requires generating all possible letter combinations that can be formed from a given sequence of digits, just like on a telephone keypad. To achieve this, we can use a backtracking approach to explore all possible combinations recursively.
+
+Approach
+We define a mapping array that holds the letters corresponding to each digit on a telephone keypad.
+We initialize an empty list to store the final result.
+We start the backtracking process by calling the recursive Backtrack function with initial parameters.
+In the Backtrack function, we check if the index is equal to the length of the input digits. 
+If it is, we have formed a valid combination and add it to the result list.
+Otherwise, we get the letters corresponding to the current digit and loop through each letter.
+For each letter, we append it to the current combination and make a recursive call with the updated combination and index+1.
+The recursion will explore all possible combinations, and each time it reaches the end of the digits, it adds the formed combination to the result list.
+Complexity
+Time complexity:
+The time complexity of this backtracking approach is O(4^N * N), where N is the length of the input digits. The maximum number of combinations that can be formed for a single digit is 4 (for digits 7, 9) since they have four letters associated with them. For other digits, there are three letters. So, in the worst case, there can be 4^N combinations, and for each combination, we copy it to the result list in O(N) time.
+
+Space complexity:
+The space complexity is O(N) as we use a recursive approach, and at any point in the recursion, the maximum depth of the call stack is equal to the length of the input digits (N). Additionally, the space used for the result list is also O(N) as it stores all the possible combinations.*/
+
 public class Solution 
 {
     private readonly string[] mapping = new string[] 
