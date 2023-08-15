9706568d6fa1b2ab01d69d376df45fd417ee06f8
Tommy Venere <veneret@gmail.com>
2023-08-14 23:56:08 -0400

    18 adding something 3

--- 1-100/18-4Sum.cs
+++ 1-100/18-4Sum.cs
@@ -14,3 +14,44 @@ Input: nums = [1,0,-1,0,-2,2], target = 0
 Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 
 */
+
+public class Solution
+{
+    public IList<IList<int>> FourSum(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        IList<IList<int>> answer = new List<IList<int>>();
+        for (int start = 0; start < nums.Length - 3; start++)
+        {
+            for (int start2 = start + 1; start2 < nums.Length - 2; start2++)
+            {
+                int left = start2 + 1;
+                int right = nums.Length - 1;
+
+                while (right > left)
+                {
+                    long sum = (long)nums[left] + (long)nums[right] + (long)nums[start2] + (long)nums[start];
+                    if (sum == target)
+                    {
+                        answer.Add(new List<int>() { nums[left], nums[right], nums[start2], nums[start] });
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (start < nums.Length - 1 && nums[start] == nums[start + 1]) start++;
+                        while (start2 < nums.Length - 1 && nums[start2] == nums[start2 + 1]) start2++;
+                        right--;
+                        left++;
+                    }
+                    else if (sum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
