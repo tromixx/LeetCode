c663655fc95c50b35855213d6cd41328f6d1588c
Tommy Venere <veneret@gmail.com>
2023-08-15 23:48:49 -0400

    19 adding something 2

--- 1-100/19-RemoveNthNodeFromEndofList.cs
+++ 1-100/19-RemoveNthNodeFromEndofList.cs
@@ -1,1 +1,11 @@
-//cositas
+/*
+Given the head of a linked list, 
+remove the nth node from the end of the list and return its head.
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+*/
