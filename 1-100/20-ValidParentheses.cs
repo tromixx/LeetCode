e7a613d152266df9a89836fd14327fc0cc4921cf
Tommy Venere <veneret@gmail.com>
2023-08-20 23:59:43 -0400

    20 commit 3

--- 1-100/20-ValidParentheses.cs
+++ 1-100/20-ValidParentheses.cs
@@ -20,3 +20,14 @@ Output: true
 */
 
 //Second Solution
+
+public class Solution {
+    public bool IsValid(string s) {
+        while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+        {
+            s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+        }
+
+        return s.Length == 0;
+    }
+}
