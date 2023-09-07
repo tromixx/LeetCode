c067e8e2fc5fc3b3004c790e45b595ff7f7bfc66
Tommy Venere <veneret@gmail.com>
2023-09-07 00:21:44 -0400

    21 commit 2

--- 1-100/21-potato.cs
+++ 1-100/21-potato.cs
@@ -1,1 +1,41 @@
-//potato
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode(-1); 
+        // Create a dummy node as the starting point
+        ListNode current = dummyHead; 
+        // Pointer to keep track of the current node
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // Attach the remaining nodes if any
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummyHead.next; 
+        // Return the merged list starting from the node after the dummy head
+
+    }
+}
