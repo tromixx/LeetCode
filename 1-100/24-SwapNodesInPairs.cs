351acdfa136ce5fe4f0a34a24ecb8688c3f97670
Tom <veneret@gmail.com>
2024-05-30 00:56:20 -0400

    nnnnnnieeeeeeeeeW

--- 1-100/24-SwapNodesInPairs.cs
+++ 1-100/24-SwapNodesInPairs.cs
@@ -1,43 +1,29 @@
-/*
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
- 
-
-Example 1:
-
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
-Example 2:
-
-Input: n = 1
-Output: ["()"]
- 
-
-Constraints:
-
-1 <= n <= 8
-*/
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 public class Solution {
-    public IList<string> GenerateParenthesis(int n) {
-        IList<string> res = new List<string>();
-         helper(res,"(",1,0,n);
-        return res;
-    }
-    public void helper(IList<string> res,string curr,int open,int close,int n)
+    public ListNode SwapPairs(ListNode head) 
     {
-        if(curr.Length==2*n)
-        {
-            res.Add(curr);
-            return;
-        }
-        if(open<n)
-        {
-           helper(res,curr+"(",open+1,close,n);
-        }
-        if(close<open)
-        {
-            helper(res,curr+")",open,close+1,n);
-        }
+        if ( (head == null) || (head.next == null) ) {return head;}                
+        ListNode node = SwapPairs(head.next.next);        
+        
+        // Swap
+        ListNode temp = head;
+        head = head.next;
+        head.next = temp;
+        
+        // Attach
+        head.next.next = node;
+        
+        return head;
+        
     }
 }
