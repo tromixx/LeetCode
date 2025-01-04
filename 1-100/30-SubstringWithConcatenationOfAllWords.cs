7b4e4b9b4e22b7c643fa3391a0afb2bf04c9ec81
“tramixx” <veneret@gmail.com>
2025-01-03 22:55:47 -0500

    Adding solutionrrree

--- 1-100/30-SubstringWithConcatenationOfAllWords.cs
+++ 1-100/30-SubstringWithConcatenationOfAllWords.cs
@@ -56,6 +56,57 @@ s and words[i] consist of lowercase English letters.
 */
 public class Solution {
     public IList<int> FindSubstring(string s, string[] words) {
-        
+        IList<int> result = new List<int>();
+        if (s == null || words == null || words.Length == 0) return result;
+
+        int wordLength = words[0].Length;
+        int wordCount = words.Length;
+        int substringLength = wordLength * wordCount;
+
+        // Create a frequency map of the words
+        Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+        foreach (var word in words) {
+            if (!wordFrequency.ContainsKey(word))
+                wordFrequency[word] = 0;
+            wordFrequency[word]++;
+        }
+
+        // Loop over each possible offset
+        for (int i = 0; i < wordLength; i++) {
+            int left = i, count = 0;
+            Dictionary<string, int> windowFrequency = new Dictionary<string, int>();
+
+            for (int right = i; right + wordLength <= s.Length; right += wordLength) {
+                string word = s.Substring(right, wordLength);
+
+                if (wordFrequency.ContainsKey(word)) {
+                    // Add the word to the window
+                    if (!windowFrequency.ContainsKey(word))
+                        windowFrequency[word] = 0;
+                    windowFrequency[word]++;
+                    count++;
+
+                    // Shrink the window if it exceeds wordCount
+                    while (windowFrequency[word] > wordFrequency[word]) {
+                        string leftWord = s.Substring(left, wordLength);
+                        windowFrequency[leftWord]--;
+                        count--;
+                        left += wordLength;
+                    }
+
+                    // Valid window found
+                    if (count == wordCount) {
+                        result.Add(left);
+                    }
+                } else {
+                    // Reset if the word is invalid
+                    windowFrequency.Clear();
+                    count = 0;
+                    left = right + wordLength;
+                }
+            }
+        }
+
+        return result;
     }
 }
