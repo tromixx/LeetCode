df345fda0075530113a7f3e91c2d52b4f2205c1a
“tramixx” <veneret@gmail.com>
2025-04-20 21:01:33 -0400

    Adding plan for algorithms

--- LeetCodeAlgorithms/TwoPointers/1-MoveZeroes.cs
+++ LeetCodeAlgorithms/TwoPointers/1-MoveZeroes.cs
@@ -1,1 +1,9 @@
 //TBD
+//REBASE
+// I could have Blepharitis
+// most likely they are not styles but Chalazia
+// I dont think dry eye or infection/bacterial
+// Maybe alergies?
+
+//Could this be related to an underlying condition like blepharitis, rosacea, or an autoimmune disease?
+
